470500922c66fe18d0da2993fba2557de8dd1923
Artem Karmaz <inste@icloud.com>
2019-03-21 19:50:36 +0200

    next commit

--- UIPageViewWithImage/PageViewController.swift
+++ UIPageViewWithImage/PageViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PageViewController: UIPageViewController {
     
+    var indexVC: Int = 0
+    var userDefaults = UserDefaults.standard
+    
     private (set) lazy var controllers: [UIViewController] = {
         return [
         UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "First"),
@@ -36,31 +39,60 @@ class PageViewController: UIPageViewController {
         
         self.view.backgroundColor = UIColor(displayP3Red: 0, green: 0.8, blue: 0.5, alpha: 0.8)
 
-
-        if let first = controllers.first {
-            setViewControllers([first], direction: .forward, animated: true, completion: nil)
-            dataSource = self
+        
+        // MARK: - User Deafult Check
+        
+        if userDefaults.object(forKey: "IndexVC") != nil {
+            // мы его вычитываем
+            self.indexVC = userDefaults.integer(forKey: "IndexVC")
+        } else {
+            userDefaults.set(0, forKey: "IndexVC")
         }
+
+        let first = controllers[indexVC]
+        print(indexVC)
+        
+        setViewControllers([first], direction: .forward, animated: true, completion: nil)
+
+    
+//        let first = controllers.first
+        dataSource = self
+
     }
 }
 
 extension PageViewController: UIPageViewControllerDataSource {
     
-    // MARK:- PageView Delegates
+    // MARK: - PageView Delegates
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = controllers.index(of: viewController) else { return nil }
-        let previousIndex = viewControllerIndex - 1
-        guard previousIndex >= 0 else { return nil }
+        guard let viewControllerIndex = controllers.firstIndex(of: viewController) else { return nil }
+//        index(of: viewController) else { return nil }
+        var previousIndex = viewControllerIndex - 1
+        
+        if previousIndex < 0 {
+            previousIndex = controllers.count - 1
+        }
+//        guard previousIndex >= 0 else { return nil }
         guard controllers.count > previousIndex else { return nil }
+        
+        self.indexVC = previousIndex
+        userDefaults.set(self.indexVC , forKey: "IndexVC")
         return controllers[previousIndex]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = controllers.index(of: viewController) else { return nil }
-        let nextIndex = viewControllerIndex + 1
-        guard controllers.count != nextIndex else { return nil }
-        guard controllers.count > nextIndex else { return nil }
+        guard let viewControllerIndex = controllers.lastIndex(of: viewController) else { return nil }
+//        index(of: viewController) else { return nil }
+        var nextIndex = viewControllerIndex + 1
+        if nextIndex == controllers.count {
+            nextIndex = 0
+        }
+//        guard controllers.count != nextIndex else { return nil }
+        self.indexVC = nextIndex
+//        guard controllers.count > nextIndex else { return nil }
+        userDefaults.set(self.indexVC , forKey: "IndexVC")
+
         return controllers[nextIndex]
     }
 }
@@ -74,9 +106,9 @@ extension PageViewController {
     }
     
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first,
-        let firstViewControllerIndex = controllers.index(of: firstViewController) else { return 0 }
-        return firstViewControllerIndex
+//        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = controllers.index(of: firstViewController) else { return 0 }
+//        return firstViewControllerIndex
+        return self.indexVC
     }
     
     
